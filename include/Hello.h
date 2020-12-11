64872b5c7a02242868d2a95e7d98831155f1086c
Ciaran Welsh <cwelsh2@uw.edu>
2020-12-11 12:26:42 +0000

    first commit of simple project for messing about with azure

---
+++ include/Hello.h
@@ -0,0 +1,21 @@
+//
+// Created by Ciaran on 11/12/2020.
+//
+
+#ifndef HELLOAZURE_HELLO_H
+#define HELLOAZURE_HELLO_H
+
+#include <iostream>
+
+class Hello {
+
+public:
+
+    Hello() = default;
+
+    std::string sayHello();
+
+};
+
+
+#endif //HELLOAZURE_HELLO_H
---
+++ src/Hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ciaran on 11/12/2020.
+//
+
+#include "Hello.h"
+
+
+std::string Hello::sayHello() {
+    return "Hello Azure";
+}
